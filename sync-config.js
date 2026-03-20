db2fe6c8704f2a9c27f84c5642548652cd04783e
arctosent <arctosent@icloud.com>
2026-03-20 19:44:39 -0300

    up

--- sync-config.js
+++ sync-config.js
@@ -1,7 +1,7 @@
 window.ARC_SYNC = {
   provider: "supabase",
-  url: "https://SEU_ID.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....",
+  url: "https://SEU_PROJECT_REF.supabase.co",
+  anonKey: "SUA_CHAVE_ANON_REAL",
   table: "site_state",
   rowId: "global",
   pollMs: 5000
