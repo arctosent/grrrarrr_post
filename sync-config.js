b87535c1331dffb2774391cb04a69845108c824d
arctosent <arctosent@icloud.com>
2026-03-20 19:50:08 -0300

    up

--- sync-config.js
+++ sync-config.js
@@ -1,7 +1,7 @@
 window.ARC_SYNC = {
   provider: "supabase",
-  url: "https://SEU_PROJECT_REF.supabase.co",
-  anonKey: "SUA_CHAVE_ANON_REAL",
+  url: "https://abcdxyz123.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....",
   table: "site_state",
   rowId: "global",
   pollMs: 5000
