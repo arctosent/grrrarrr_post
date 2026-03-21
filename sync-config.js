af6b0d77f33570ac2d6a95d6087bb5178d4f9dc2
arctosent <arctosent@icloud.com>
2026-03-21 13:34:36 -0300

    novo site

--- sync-config.js
+++
@@ -1,8 +0,0 @@
-window.ARC_SYNC = {
-  provider: "supabase",
-  url: "https://SEU_PROJECT_REF_REAL.supabase.co",
-  anonKey: "SUA_ANON_KEY_REAL",
-  table: "site_state",
-  rowId: "global",
-  pollMs: 5000
-};
